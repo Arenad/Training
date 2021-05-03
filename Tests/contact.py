ccde88f8be3c596e488ce4ede82f9c302941c2fd
Arenad <darkfootman@gmail.com>
2021-05-03 17:55:01 +0300

    Тест контактов

---
+++ Tests/contact.py
@@ -0,0 +1,31 @@
+__author__ = "Artur"
+
+
+class Contact:
+    def __init__(self, firstname, middlename, nickname, lastname, title, company, address, home, mobile,
+                 email, work, fax, email2, email3, homepage, bday, bmonth, byear, aday, amonth, ayear,
+                 address2, phone2, notes):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.nickname = nickname
+        self.lastname = lastname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.address2 = address2
+        self.home = home
+        self.mobile = mobile
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.work = work
+        self.fax = fax
+        self.homepage = homepage
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.aday = aday
+        self.amonth = amonth
+        self.ayear = ayear
+        self.phone2 = phone2
+        self.notes = notes
